b6756a2fdc1f70be1872388dcec99cf09659a429
Sakshi Bhokare <bhokaresakshi93@gmail.com>
2024-03-06 18:38:58 +0530

    weather app

---
+++ MiniProject-2/weatherApp.js
@@ -0,0 +1,49 @@
+const API_KEY = "398c5cf612e3ae6de6577bb64818caa3";
+
+
+function renderWeatherInfo(data){
+    let newPara = document.createElement('p');
+    newPara.textContent = `${data?.main?.temp.toFixed(2)} ℃`;
+    document.body.appendChild(newPara);
+}
+
+
+async function fetchWeatherDetails(){
+
+    try{
+        let city='goa';
+
+        const response= await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`);
+    
+        const data=await response.json();
+        console.log("Weather data:-> ", data);
+    
+
+        renderWeatherInfo(data);
+        // let newPara = document.createElement('p');
+        // newPara.textContent = `${data?.main?.temp.toFixed(2)} ℃`;
+        // document.body.appendChild(newPara);
+    }
+    catch(err){
+         //handle the error here
+
+    }
+    
+}
+
+async function getCustomWeatherDetails(){
+    try{
+        let lat=17;
+        let lon = 18;
+     
+        let result =await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
+    
+        let data = await result.json();
+    
+        console.log(data)
+    }
+    catch(e){
+        console.log("Error is occured", e);
+    }
+    
+}
